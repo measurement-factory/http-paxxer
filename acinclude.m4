dbf5882a7d33138d4bcc942c9087bd2830a0dd4b
Alex Rousskov <rousskov@measurement-factory.com>
2015-01-10 13:03:52 -0700

    Remove AX_* macros from cfgaux/, use system versions instead.
    Autoconf-archive package is now required for bootstrapping. Should not be an
    issue since users usually build from bootstrapped tarballs.
    
    To protect HttpPaxxer developers, we now also fail bootstrapping if macros
    from Autoconf Macros Archive are found in configure. Autoconf is supposed to
    expand Archive macros when they are installed, but it does not fail if it does
    not know about those macros, and ./configure later fails in various, often
    misleading, ways. We now fail at bootstrapping time with an appropriate error
    message.
    
    The main benefit is that we do not need to manually update the bundled
    macros.

--- acinclude.m4
+++
@@ -1,3 +0,0 @@
-dnl import a macro to prefix config.h defines with a package name
-dnl http://ac-archive.sourceforge.net/Miscellaneous/ax_prefix_config_h.html
-m4_include(cfgaux/ax_prefix_config_h.m4)
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,12 @@
 # only developers might need to run this script
 
 autoreconf --force --install
+
+# leave grep output on the console to supply macro details.
+if grep -E 'AX_PREFIX_CONFIG_H' configure; then
+    echo "Error: Macros from Autoconf Macro Archive not expanded."
+    echo "       Do you need to install the Archive?"
+    echo "       Moving configire script to configure.invalid for now."
+    mv -i configure configure.invalid
+    exit 1
+fi
